64ac2e06cc109e7e303e9434e732e8397b3ff6d0
Lucas Wilkerson <email@example.com>
2023-10-30 02:12:07 -0700

    Finished with Mosh

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,20 @@
-import ListGroup from './components/ListGroup';
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup/></div>
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          <strong>Holy guacamole!</strong> The button worked!
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>Press Me!</Button>
+    </div>
+  );
 }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -5,16 +5,36 @@
 // dynamically populate a list.
 // Line 15 hard to explain, see 38:20 for good explanation
 
-function ListGroup() {
-  let items = ["New York", "Corona", "Arcata", "Eastvale"];
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  // (item: string) => void
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
